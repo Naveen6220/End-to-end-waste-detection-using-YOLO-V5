966595aba4500ae6ab66c4f1fccc121f03a4a5bd
Narayanan <146112960+Naveen6220@users.noreply.github.com>
2023-11-29 02:38:59 +0530

    model trainer added

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from setuptools import find_packages, setup
 
 setup(
@@ -8,18 +7,3 @@ setup(
     author_email= 'naveeneen02@gmail.com',
     packages= find_packages(),
     install_requires = []
-
-)
-=======
-from setuptools import find_packages, setup
-
-setup(
-    name = 'wasteDetection',
-    version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
-    packages= find_packages(),
-    install_requires = []
-
-)
->>>>>>> 8e97f063dc3e5bdec85f016422638f0eaeb41086
