2531efdc334671cb1f8101eee4e3e8c43f7e814d
Naveen6220 <nivinkumar7197@gmail.com>
2023-11-29 02:28:08 +0530

    updated requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Naveen N',
+    author_email= 'naveeneen02@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
